d9216af79a00c4dde9cf3095f13fafb9c35fa939
polantsev <polyantsevyury@gmail.com>
2024-08-25 17:15:02 +0200

    Finish "guessing game" chapter;

--- rust_book/guessing_game/src/main.rs
+++ rust_book/guessing_game/src/main.rs
@@ -1,16 +1,38 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Input your guess");
-    let mut guess = String::new();
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    loop {
+        println!("Input your guess");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess)
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+    }
 }
